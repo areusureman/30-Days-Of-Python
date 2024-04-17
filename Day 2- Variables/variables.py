340edfa6e16b633ec1706a1cd03a2a7f3e1b8edb
areusureman <96713404+areusureman@users.noreply.github.com>
2024-04-17 15:10:26 +0000

    variables

---
+++ Day 2- Variables/variables.py
@@ -0,0 +1,30 @@
+#Day 2: 30 Days of python programming
+
+first_name = 'John'
+last_name = 'Doe'
+full_name = 'John Doe'
+country = 'THE united states of america'
+city = 'New York'
+age = 16
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+skills = ['hunting', 'fishing', 'swimming']
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(skills))
+
+print(len(first_name))
+print(len(last_name))
+
+print(len(first_name) + len(last_name))
